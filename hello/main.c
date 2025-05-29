20158a992a1e5cfdc0839088d4605f09c8e02b8f
Lincoln <lincolncpp@gmail.com>
2025-05-28 21:58:40 -0300

    add hello world

---
+++ hello/main.c
@@ -0,0 +1,23 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 Lincoln <lincolncpp@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Lincoln <lincolncpp@gmail.com>
+ */
+
+#include <stdio.h>
+
+int main(void)   
+{
+  printf ("Hello World");
+  return 0;
+}
+
+
+
+
+
---
+++ hello/stdio.h
@@ -0,0 +1,16 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 Lincoln <lincolncpp@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Lincoln <lincolncpp@gmail.com>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *); 
+
+#endif	
