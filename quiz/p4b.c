7b7f518c3e3d6d2be2435fa8dca6773dbfea17c5
Fernando Lincoln <lincolncpp@gmail.com>
2025-07-06 12:21:30 -0300

    add quiz task

---
+++ quiz/p4b.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 Fernando Lincoln <lincolncpp@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Fernando Lincoln <lincolncpp@gmail.com>
+ */
+
+#include <stdio.h>
+
+void bar()
+{
+  printf ("Bar\n");
+}
